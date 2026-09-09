33ed61629d71ffb2518d19eae164f8b2a8636da0
ImJustPeet <donovanpeet@gmail.com>
2026-09-09 19:13:38 +0200

    test/3D: temp debug hook

--- test.js
+++ test.js
@@ -596,6 +596,9 @@ function initScene() {
     io.observe(heroEl);
   }
 
+  // TEMP debug hook
+  window.__t3d = { renderer, scene, camera, get model() { return model; } };
+
   // ---- go ----
   function boot() {
     // repaint canvas textures once webfonts are ready so the mono font shows
